922f3116c89398f7649aeccb40b565629bc864f5
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-08-02 18:41:01 +0000

    add times method

--- dice.rb
+++ dice.rb
@@ -13,47 +13,66 @@ end
 get("/zebra") do
   "We must add a route for each path we want to support"
 end
+
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @rolls = []
+
+  2.times do
+    dice = rand(1..6)
+
+    @rolls.push(dice)
+  end
 	erb(:two_six)
 end
 
 
 get("/dice/2/10") do
-  first_one = rand(1..10)
-  second_one = rand(1..10)
-  total = first_one + second_one
+  @rolls = []
 
-  @output = "You rolled a #{first_one} and a #{second_one} for a total of #{total}."
+  2.times do
+    dice = rand(1..10)
 
+    @rolls.push(dice)
+  end
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
-only_one = rand(1..20)
+  @rolls = []
 
-@output = "You rolled a total of #{only_one}."
+  1.times do
+    dice = rand(1..20)
+
+    @rolls.push(dice)
+  end
 
 erb(:one_tweny)
 end
 
 get("/dice/5/4") do
-  first = rand(1..4)
-  second = rand(1..4)
-  third = rand(1..4)
-  fourth = rand(1..4)
-  fifth = rand(1..4)
- 
-  total = first + second + third + fourth + fifth
-  @output = "You rolled a #{first},  a #{second}, a #{third}, and two anothers #{fourth} and  #{fifth} for a total of #{total}."
+  @rolls = []
+
+  5.times do
+    dice = rand(1..4)
+
+    @rolls.push(dice)
+  end
 
   erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
